886e347ea1311f2ba74c15cd24ca19526bc0574e
Daniel Murrmann <9040811+fancyDevelopment@users.noreply.github.com>
2025-01-20 12:45:35 +0100

    Added custom header

--- Frontend/apps/src/app/app.config.ts
+++ Frontend/apps/src/app/app.config.ts
@@ -3,12 +3,12 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
-import { provideHateoas, withAntiForgery, withLoginRedirect } from '@angular-architects/ngrx-hateoas';
+import { provideHateoas, withAntiForgery, withCustomHeaders, withLoginRedirect } from '@angular-architects/ngrx-hateoas';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
     provideHttpClient(),
-    provideHateoas(withLoginRedirect(), withAntiForgery())
+    provideHateoas(withLoginRedirect(), withAntiForgery(), withCustomHeaders({headers:{"X-Requested-With": 'XmlHttpRequest'}}))
   ]
 };
